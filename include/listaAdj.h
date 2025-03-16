8fbce2a308b2eb3b28f99225c33a01221dafdf7f
Daniel Thielmann <thielmanndan@gmail.com>
2025-03-16 13:09:20 -0300

    bug

--- include/listaAdj.h
+++ include/listaAdj.h
@@ -7,23 +7,27 @@ struct NoAdj {
     int destino;
     int peso;
     NoAdj* prox;
-
     NoAdj(int d, int p) : destino(d), peso(p), prox(nullptr) {}
 };
 
 class ListaAdj {
-private:
-    NoAdj** lista; // Vetor de ponteiros para listas encadeadas
-    int numVertices;
 
-public:
-    ListaAdj();  
-    ListaAdj(int numVertices);
-    ~ListaAdj();
-    void inserirAresta(int origem, int destino, int peso);
-    void imprimir() const;
-    int getCusto(int origem, int destino) const;
-    std::pair<int, int>* obterVizinhos(int vertice, int& tamanho) const;
-};
+    private:
+    
+        NoAdj** lista;
+        int numVertices;
+    
+    public:
+
+   
+
+
+        ListaAdj();
+        ListaAdj(int numVertices);
+        void inserirAresta(int origem, int destino, int peso);
+        void imprimir() const;
+        int getCusto(int origem, int destino) const; // Novo método
+        ~ListaAdj();
+    };
 
 #endif // LISTA_ADJ_H
